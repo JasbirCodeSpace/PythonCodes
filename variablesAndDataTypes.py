213eca3983765bb1a81ac67d371f0994f2d32e95
Jasbir <shikhawat.jasbir@gmail.com>
2019-08-12 21:49:42 +0530

    Add files via upload

---
+++ variablesAndDataTypes.py
@@ -0,0 +1,15 @@
+variableNamesCannotBeginWithANumber = True
+initial = 3
+boolean = False
+posInt = 123
+zero = 0
+negFloat = -1.23
+initial = "This is a reassignment"
+
+print(initial , boolean)
+
+# single line comment
+'''
+Docstring example
+'''
+
