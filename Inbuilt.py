6a93a2af97319a03d9850562160e7e238bbbb205
Jasbir <shikhawat.jasbir@gmail.com>
2019-08-12 21:51:29 +0530

    Add files via upload

---
+++ Inbuilt.py
@@ -0,0 +1,27 @@
+"""
+abs() and type():
+1.use abs() to get and print the absolute value of a positive number
+2.use abs() to get and print the absolute value of a negative number
+3.use type() to get and print the type of a float
+4.use type() to get and print the type of an integer
+5.use type() to get and print the type of a boolean value
+6.use type() to get and print the type of a string
+"""
+
+print(abs(int(input("Enter a positive number : "))))
+print(abs(int(input("Enter a negative number : "))))
+
+print(type(float(input("Enter a float number : "))))
+print(type(int(input("Enter an integer number : "))))
+print(type(bool(input("Enter a boolean value : "))))
+print(type(input("Enter a string: ")))
+
+"""
+max() and min():
+1.use max() to get and print the largest of inputs of your choosing that are all integers
+2.use max() to get and print the alphabetical maximum of inputs of your choosing that are all strings
+3.use min() to get and print the smallest of inputs of your choosing that are all floats
+4.use min() to get and print the alphabetical minimum of inputs of your choosing that are all strings
+"""
+
+
